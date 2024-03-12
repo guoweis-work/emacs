67128ea3d6a2db3f30fba103b35fcff13951928e
Guowei Shieh <guoweis@meta.com>
2024-03-11 19:59:22 -0700

    fix a typo on git path

--- dependencies.sh
+++ dependencies.sh
@@ -3,4 +3,4 @@
 mkdir ~/git
 git clone https://github.com/konrad1977/welcome-dashboard ~/git/konrad1977_welcome-dashboard
 git clone https://github.com/JasZhe/window-stool ~/git/JasZhe_window-stool
-git clone https://github.com/jdtsmish/eglot-booster ~/git/jdtsmith_eglot-booster
+git clone https://github.com/jdtsmith/eglot-booster ~/git/jdtsmith_eglot-booster
