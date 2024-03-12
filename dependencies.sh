50fa0c9127bdfd84c32496b9e6e9fbba033ce731
Guowei Shieh <guoweis@meta.com>
2024-03-11 20:08:15 -0700

    add emacs-lsp-booster

--- dependencies.sh
+++ dependencies.sh
@@ -4,3 +4,9 @@ mkdir ~/git
 git clone https://github.com/konrad1977/welcome-dashboard ~/git/konrad1977_welcome-dashboard
 git clone https://github.com/JasZhe/window-stool ~/git/JasZhe_window-stool
 git clone https://github.com/jdtsmith/eglot-booster ~/git/jdtsmith_eglot-booster
+
+# emacs-lsp-booster
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+git clone https://github.com/blahgeek/emacs-lsp-booster ~/git/blahgeek_emacs-lsp-booster
+cd ~/git/blahgeek_emacs-lsp-booster/
+cargo build --release
